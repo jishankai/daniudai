40b91971ccb0c2745f916967d9251b423d4785c1
skji <jishankai@gmail.com>
2015-05-22 16:33:52 +0800

    UPDATE

--- frontend/controllers/LoanController.php
+++ frontend/controllers/LoanController.php
@@ -2,30 +2,68 @@
 
 namespace frontend\controllers;
 
+use Yii;
+use Overtrue\Wechat\Auth;
+use backend\models\User; 
+
 class LoanController extends \yii\web\Controller
 {
     public function actionBank()
     {
-        $this->layout = FALSE;
-        return $this->render('bank');
+        $user = $_SESSION['user'];
+        $u = User::findOne($user['openid']);
+
+        return $this->renderPartial('bank', array('user'=>$u));
     }
 
     public function actionIndex()
     {
-        $this->layout = FALSE;
-        return $this->render('index');
+        $appid = Yii::$app->params['wechat_appid'];
+        $appsecret = Yii::$app->params['wechat_appsecret'];
+
+        if (empty($_SESSION['user'])) {
+            $auth = new Auth($appid, $appsecret);
+            $user = $auth->authorize($to = null, $scope = 'snsapi_userinfo', $state = 'STATE'); // 返回用户 Bag
+            $_SESSION['user'] = $user;
+        } else {
+            $user = $_SESSION['user'];
+        }
+        $open_id = $user['openid'];
+
+        $u = User::findOne($open_id);
+        if (!isset($u)) {
+            $transaction = $db->beginTransaction();
+            try {
+                $u = new User;
+                $u->wechat_id = $open_id;
+                $u->created_at = time();
+                $u->save();
+                $transaction->commit();
+            } catch(\Exception $e) {
+                $transaction->rollBack();
+                throw $e;
+            }
+        } else {
+            $l = Loan::findOne($open_id);
+            if (!isset($l) or $l->status!=0) {
+                $this->redirect('loan/success');
+            }
+        }
+        
+        return $this->renderPartial('index');
     }
 
     public function actionSchool()
     {
-        $this->layout = FALSE;
-        return $this->render('school');
+        $user = $_SESSION['user'];
+        $school = School::findOne($user['openid']);
+
+        return $this->renderPartial('school', array('school'=>$school));
     }
 
     public function actionSuccess()
     {
-        $this->layout = FALSE;
-        return $this->render('success');
+        return $this->renderPartial('success');
     }
 
 }
