75fe6eaeef1204f8eaff743ba8860f52a6007b81
skji <jishankai@gmail.com>
2015-05-27 03:42:10 +0800

    UPDATE

--- frontend/controllers/WechatController.php
+++ frontend/controllers/WechatController.php
@@ -9,7 +9,7 @@ class WechatController extends \yii\web\Controller
 {
     public function actionMessage()
     {
-        $appid = Yii::$app->params['wechat_appid'];
+        $appId = Yii::$app->params['wechat_appid'];
         $secret = Yii::$app->params['wechat_appsecret'];
         $token = Yii::$app->params['wechat_token'];
         $encodingAESKey = Yii::$app->params['wechat_aeskey'];
