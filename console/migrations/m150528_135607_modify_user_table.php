dc62bc502de553f1a4ed6b9a8ddaa88943ca74ea
skji <jishankai@gmail.com>
2015-05-28 22:12:18 +0800

    UPDATE

---
+++ console/migrations/m150528_135607_modify_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150528_135607_modify_user_table extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('user', 'mobile', Schema::TYPE_STRING . '(11) NOT NULL DEFAULT ""');
+        $this->alterColumn('user', 'bank_id', Schema::TYPE_STRING . '(24) NOT NULL DEFAULT ""');
+    }
+
+    public function down()
+    {
+        $this->alterColumn('user', 'bank_id', Schema::TYPE_INTEGER . '(20) NOT NULL DEFAULT ""');
+        $this->alterColumn('user', 'mobile', Schema::TYPE_INTEGER . '(11) NOT NULL DEFAULT ""');
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
