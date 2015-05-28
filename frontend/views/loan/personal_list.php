12ddebc7ea72334e92e37f877af9cb0a165c784d
skji <jishankai@gmail.com>
2015-05-28 16:58:34 +0800

    UPDATE

---
+++ frontend/views/loan/personal_list.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width = device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>跟踪1</title>
+	<style type="text/css">
+		th{height: 30px;}
+		td{height: 26px;text-align: center;line-height: 26px;}
+	</style>
+</head>
+<body>
+	<table border="1" cellpadding="0" cellspacing="0" width="100%">
+		<tr>
+			<th>姓 名</th>
+			<th>专 业</th>
+			<th>手机号</th>
+		</tr>
+		<tr>
+			<td>伊丁</td>
+			<td>护理</td>
+			<td>189xxxxxxxx</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+</body>
+</html>
