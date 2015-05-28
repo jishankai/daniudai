a6d1fdabda2b9f5a6c523c8871bb4eaf442c1b03
hez <enzihuang@163.com>
2015-05-28 18:53:17 +0800

    dorm

--- frontend/views/loan/school.php
+++ frontend/views/loan/school.php
@@ -75,7 +75,7 @@
 			                <div class="forms__group">
 			                    <label class="forms__label">详细地址</label>
 			                    <span class="input__box no-border">   
-			                        <input type="text" class="forms_input" placeholder="宿舍楼和房间号" id="address" name="address">
+			                        <input type="text" class="forms_input" placeholder="宿舍楼和房间号" id="address" name="dorm">
 			                        <span class="icon-option"><i class="icons icons-cross"></i></span>
 			                    </span>
 			                </div>
