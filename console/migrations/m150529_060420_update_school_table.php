3f5029539b824906e1538a13b202632376d6aad8
skji <jishankai@gmail.com>
2015-05-29 14:07:28 +0800

    UPDATE

---
+++ console/migrations/m150529_060420_update_school_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150529_060420_update_school_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
