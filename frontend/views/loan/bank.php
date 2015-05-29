dd1fdf180ffb329e53209e9208397172ba3c9e52
hez <enzihuang@163.com>
2015-05-29 19:42:51 +0800

    修改1

--- frontend/views/loan/bank.php
+++ frontend/views/loan/bank.php
@@ -46,15 +46,15 @@
 			                    <label class="forms__label">卡号</label>
 			                    <span class="input__box">             
 			                        <input type="text" class="forms_input" placeholder="银行卡号" value="" id="bc-num" name="bank_id">
-			                        <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i1"></i></span>
-			                    </span>
+			                        <!-- <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i1"></i></span>
+			                    </span> -->
 			                </div>
 			                <div class="forms__group">
 			                    <label class="forms__label">确认卡号</label>
 			                    <span class="input__box no-border"> 
 			                        <input type="text" class="forms_input" placeholder="确认银行卡号"  value="" id="cbc-num">
-			                        <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i2"></i></span>
-			                    </span>
+			                        <!-- <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i2"></i></span>
+			                    </span> -->
 			                </div>
 			            </div>
 			            <div class="forms-item">
@@ -70,15 +70,15 @@
 			                    <label class="forms__label">身份证</label>
 			                    <span class="input__box">           
 			                        <input type="text" class="forms_input" placeholder="身份证号" value="" id="id-num" name="id">
-			                        <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i4"></i></span>
-			                    </span>
+			                        <!-- <span class="icon-option"><i class="icons icons-cross" style="display:block;" id="i4"></i></span>
+			                    </span> -->
 			                </div> 
 			                <div class="forms__group">
 			                    <label class="forms__label">手机号</label>
 			                    <span class="input__box no-border">   
 			                        <input type="text" class="forms_input" placeholder="预留在银行的手机号"  id="phone-num" name="mobile">
-			                        <span class="icon-option"><i class="icons icons-cross"style="display:block;" id="i5"></i></span>
-			                    </span>
+			                        <!-- <span class="icon-option"><i class="icons icons-cross"style="display:block;" id="i5"></i></span>
+			                    </span> -->
 			                </div> 
 		                </div>
 		                <div class="forms__option">
