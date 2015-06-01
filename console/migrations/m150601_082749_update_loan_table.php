3cdbf8372691980ab1ac291d555b6ba994af0cb7
skji <jishankai@gmail.com>
2015-06-01 16:56:43 +0800

    UPDATE

--- console/migrations/m150601_082749_update_loan_table.php
+++ console/migrations/m150601_082749_update_loan_table.php
@@ -10,8 +10,8 @@ class m150601_082749_update_loan_table extends Migration
         $this->addColumn('loan', 'reviewer', Schema::TYPE_STRING . '(45) NOT NULL DEFAULT "" AFTER status');
         $pku101_supporter = Yii::$app->params['pku101_supporter'];
         $pku102_supporter = Yii::$app->params['pku102_supporter'];
-        Yii::$app->db->createCommand("UPDATE loan l, student s SET reviewer={$pku101_supporter} WHERE l.wechat_id=s.wechat_id AND s.school_id LIKE '101%'")->execute();        
-        Yii::$app->db->createCommand("UPDATE loan l, student s SET reviewer={$pku102_supporter} WHERE l.wechat_id=s.wechat_id AND s.school_id LIKE '102%'")->execute();        
+        Yii::$app->db->createCommand("UPDATE loan l, student s SET reviewer='{$pku101_supporter}' WHERE l.wechat_id=s.wechat_id AND s.school_id LIKE '101%'")->execute();        
+        Yii::$app->db->createCommand("UPDATE loan l, student s SET reviewer='{$pku102_supporter}' WHERE l.wechat_id=s.wechat_id AND s.school_id LIKE '102%'")->execute();        
     }
 
     public function down()
