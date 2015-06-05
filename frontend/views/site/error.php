85dc3b98d5575e8218599d66723378c9052482c3
skji <jishankai@gmail.com>
2015-06-05 17:52:32 +0800

    UPDATE

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -1,27 +1,44 @@
-<?php
-
-use yii\helpers\Html;
-
-/* @var $this yii\web\View */
-/* @var $name string */
-/* @var $message string */
-/* @var $exception Exception */
-
-$this->title = $name;
-?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+<!DOCTYPE html>
+<html class="mobile-notes-variant" lang="en"><!--full-srceen-->
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" user-scalable="no">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>404</title>
+<style type="text/css">
+*{margin: 0;padding: 0;}
+body{background-color: #B0B0B0;}
+.wrap{width: 380px;height: 100%;margin: 0 auto;margin-top: 50px;}
+img{width: 250px;}
+@media only screen and (min-width: 300px) and (max-width: 650px){
+.wrap{width:100%;height:100%;background: url("img/wu.jpg")left top;margin-top: 0;}
+.hr_20{width: 100%;height: 5%;}
+img{width:50%;display: block;margin: 0 auto;margin-top: 5%;}	
+h1{font-size:1.8rem;line-height: 1.5;text-align: center;}
+h3{margin:5% 10%; line-height: 1.2;}
+ul{margin: 1% 15%;}
+li{line-height: 1.2;}
+}
+</style>
+</head>
+<body>
+<div class="wrap">
+	<div class="hr_20"></div>
+	<h1>雾霾太大，</h1>
+	<h1>连页面都看不见啦？！</h1>
+	<img src="img/bug_cows.png">
+	<div>
+		<h3>没有找到您要访问的网页，原因可能如下：</h3>
+		<ul>
+			<li>可能网速过慢，或网络中断</li>
+			<li>可能页面已被删除</li>
+			<li>这个网页太受欢迎了，太多人在访问</li>
+		</ul>
+	</div>
+</div>	
+</body>
+<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+<script type="text/javascript">
+	$(".wrap").height($(window).height());
+</script>
+</html>
