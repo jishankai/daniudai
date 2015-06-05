42b73ac945a22e9407145cd928dcac8bb26c24b1
skji <jishankai@gmail.com>
2015-06-05 18:01:30 +0800

    UPDATE

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -55,14 +55,21 @@ class SiteController extends Controller
     public function actions()
     {
         return [
+            /*
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
+            */
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
+    }
+
+    public function actionError()
+    {
+        return $this->renderPartial('error');    
     }
 
     public function actionIndex()
