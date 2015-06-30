131c5c3006f90084fa5da2c3a4b10dae66bc246c
skji <jishankai@gmail.com>
2015-06-30 15:01:17 +0800

    UPDATE

--- frontend/controllers/LoanController.php
+++ frontend/controllers/LoanController.php
@@ -69,7 +69,8 @@ class LoanController extends \yii\web\Controller
         if (isset($l) AND $l->status>0) {
             return $this->redirect(['loan/success']);
         }
-        $rate = ($type=='common')?0.0002:0.0001;
+        //$rate = ($type=='common')?0.0002:0.0001;
+        $rate = 0.0003;
         $appId = Yii::$app->params['wechat_appid'];
         $secret = Yii::$app->params['wechat_appsecret'];
         $js = new Js($appId, $secret); 
@@ -329,6 +330,18 @@ class LoanController extends \yii\web\Controller
                     "remark"   => "我们会在 24 小时内给您汇款，请耐心等待。",
                 );
                 $messageId = $notice->uses($templateId)->withUrl($url2)->andData($data2)->andReceiver($l->wechat_id)->send();
+            } else if ($operation==-1) {
+                $templateId = Yii::$app->params['templateId_review'];
+                $url = Url::to(['loan/failed`'],TRUE);
+                $data = array(
+                    "first"    => "大牛您好！您没有通过审核",
+                    "keyword1" => "{$l->money}元",
+                    "keyword2" => "{$l->duration}天",
+                    "keyword3" => "{$l->rate}*每个月的天数",
+                    "keyword4" => "未通过",
+                    "remark"   => "请核对借款条件及个人信息后再提出申请，谢谢关注。",
+                );
+                $messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($l->wechat_id)->send();
             }
         } else if ($operation==3 AND $open_id==Yii::$app->params['admin_supporter']) {
             $l = Loan::findOne($loan_id);
