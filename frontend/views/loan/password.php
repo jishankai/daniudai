95f61bc1f5c9de53544cf7b2e6f6c1b7ed43672a
skji <jishankai@gmail.com>
2015-07-09 16:06:51 +0800

    update

--- frontend/views/loan/password.php
+++ frontend/views/loan/password.php
@@ -68,7 +68,7 @@
 <script type="text/javascript" src="js/widgets/MessageBox.js?<?php echo $v; ?>"></script>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript">
-$("#container").PUB();
+$("#container").PUC();
 wx.config(<?php echo $js->config(array('hideOptionMenu'), false, true) ?>);
 wx.ready(function(){
     wx.hideOptionMenu();
