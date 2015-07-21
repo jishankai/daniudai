6ea54f2b2272a5f489cb3ae7fc2ef2e886fa3a3a
skji <jishankai@gmail.com>
2015-07-21 18:33:53 +0800

    update repay

--- frontend/controllers/LoanController.php
+++ frontend/controllers/LoanController.php
@@ -439,13 +439,13 @@ class LoanController extends \yii\web\Controller
 
         $open_id = $user['openid'];
         if ($open_id==Yii::$app->params['pku101_supporter']) {
-            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE stu.school_id LIKE "101%" AND l.status>=1')->queryAll();
+            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE stu.school_id LIKE "101%" AND l.status>=1 ORDER BY l.status ASC, l.update_time DESC')->queryAll();
             return $this->renderPartial('personal_list',['r'=>$r]);
         } else if($open_id==Yii::$app->params['pku102_supporter']) {
-            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE stu.school_id LIKE "102%" AND l.status>=1')->queryAll();
+            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE stu.school_id LIKE "102%" AND l.status>=1 ORDER BY l.status ASC, l.update_time DESC')->queryAll();
             return $this->renderPartial('personal_list',['r'=>$r]);
         } else if($open_id==Yii::$app->params['demo_supporter']) {
-            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE l.status>=1')->queryAll();
+            $r = Yii::$app->db->createCommand('SELECT u.name,u.mobile,s.depart, l.loan_id,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN student stu ON u.wechat_id=stu.wechat_id LEFT JOIN school s ON stu.school_id=s.school_id WHERE l.status>=1 ORDER BY l.status ASC, l.update_time DESC')->queryAll();
             return $this->renderPartial('personal_list',['r'=>$r]);
             //$r = Yii::$app->db->createCommand('SELECT u.name,u.bank,u.bank_id, l.loan_id,l.money,l.duration,t.name AS reviewer,l.status FROM user u LEFT JOIN loan l ON u.wechat_id=l.wechat_id LEFT JOIN user t ON l.reviewer=t.wechat_id WHERE l.status=2')->queryAll();
             //return $this->renderPartial('bank_list', ['verification'=>'demo','r'=>$r]);
@@ -614,5 +614,20 @@ class LoanController extends \yii\web\Controller
 
         $js = new Js($appId, $secret); 
         return $this->renderPartial('password', ['v'=>Yii::$app->params['assets_version'], 'type'=>$type, 'js'=>$js]);
+    }
+
+    public function actionRepays()
+    {
+        return $this->renderPartial('repay_list');
+    }
+
+    public function actionRepay()
+    {
+        return $this->renderPartial('repay');
+    }
+
+    public function actionRepayed()
+    {
+        return $this->renderPartial('repayed');
     }
 }
---
+++ frontend/views/loan/repay.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html class="mobile-notes-variant bg-color-white" lang="en"><!--full-srceen-->
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" user-scalable="no">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>贷款详情</title>
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-precomposed-144.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-precomposed-114.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/logo-72.png">
+<link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/base.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/fonts.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/widget.css" />
+</head>
+<body>
+	<div class="container">
+		<div class="screen-content">			
+			<div class="content">
+				<div class="repay-box">	
+					<div class="repay-text">
+						<div class="r-name">到期还款日</div>
+						<div class="r-time font-green">还剩2天</div>
+						<!--div class="r-time font-red">已逾期2天</div>
+						<div class="r-time font-gray">还剩2天</div-->
+						<span>2015.07.02</span>
+					</div>
+					<div class="repay-text">
+						<div class="r-name">应还总额</div>						
+						<span class="r-money">1002&nbsp;元</span>
+						<div class="r-money-s">
+							本金<span class="s-money">1000</span>
+						</div>
+						<div class="r-money-s">
+							利息<span class="s-money">2</span>
+						</div>
+						<div class="r-money-s">
+							罚息<span class="s-money">0</span><!--font-red-->
+						</div>
+					</div>
+					<div class="repay-text">
+						<div class="r-name">借款日期</div>						
+						<span>2015.04.13</span>
+					</div>
+					<div class="repay-text">
+						<div class="r-name">周期</div>						
+						<span>100&nbsp;天</span>
+					</div>					
+	                <div class="repay-btn">
+	                	<button class="btn btn-orange btn-fullwidth">立刻还款</button>
+	                </div>
+	                <p class="repay-xy"><a href="#">查看《借款协议》</a></p>
+				</div>
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
