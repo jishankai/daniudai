99a1739c36be80d0e043ae9b76e3c24fc1ab9ae7
skji <jishankai@gmail.com>
2015-07-29 15:15:10 +0800

    update reminder

---
+++ console/controllers/ReminderController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace console\controllers;
+ 
+use yii\console\Controller;
+use yii\helpers\Url;
+use Overtrue\Wechat\Notice;
+use backend\models\Loan; 
+ 
+class ReminderController extends Controller {
+ 
+    public function actionIndex() {
+        echo "Reminder Working...";
+
+        $loans = Loan::findAll(['status'=>3]);
+        foreach ($loans as $l) {
+            $appId = Yii::$app->params['wechat_appid'];
+            $secret = Yii::$app->params['wechat_appsecret'];
+
+            $notice = new Notice($appId, $secret);
+            $templateId = Yii::$app->params['templateId_repay_remind'];
+            $url = Url::to(['loan/repay'],TRUE);
+            $data = array(
+                "first"    => "您有一笔借款即将到期，请注意及时还款。",
+                "keyword1" => date("Y-m-d", $l->end_at),
+                "keyword2" => $l->money,
+                "keyword3" => $l->money*$l->rate*$l->duration,
+                "remark"   => "",
+            );
+            $messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($l->wechat_id)->send();
+        }
+
+        echo "Reminder Stop";
+
+    }
+
+}
