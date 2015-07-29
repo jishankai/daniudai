4a9c7d45311e61cbfef961c57095596cd7248aa6
skji <jishankai@gmail.com>
2015-07-29 16:04:11 +0800

    update reminder

--- console/controllers/ReminderController.php
+++ console/controllers/ReminderController.php
@@ -13,25 +13,28 @@ class ReminderController extends Controller {
     public function actionIndex() {
         echo "Reminder Working...\n";
 
+        $appId = Yii::$app->params['wechat_appid'];
+        $secret = Yii::$app->params['wechat_appsecret'];
+
         $loans = Loan::findAll(['status'=>3]);
         foreach ($loans as $l) {
-            $appId = Yii::$app->params['wechat_appid'];
-            $secret = Yii::$app->params['wechat_appsecret'];
-
-            $notice = new Notice($appId, $secret);
-            $templateId = Yii::$app->params['templateId_repay_remind'];
-            $url = Url::to(Yii::$app->params['host'].'/index.php?r=loan/repay');
-            $data = array(
-                "first"    => "您有一笔借款即将到期，请注意及时还款。",
-                "keyword1" => date("Y-m-d", $l->end_at),
-                "keyword2" => $l->money,
-                "keyword3" => $l->money*$l->rate*$l->duration,
-                "remark"   => "",
-            );
-            $messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($l->wechat_id)->send();
+            $t = $l->end_at-time();
+            if (($t<7*24*3600 and $t>=6*24*3600) or $t<24*3600) {
+                $notice = new Notice($appId, $secret);
+                $templateId = Yii::$app->params['templateId_repay_remind'];
+                $url = Url::to(Yii::$app->params['host'].'/index.php?r=loan/repay');
+                $data = array(
+                    "first"    => "您有一笔借款即将到期，请注意及时还款。",
+                    "keyword1" => date("Y-m-d", $l->end_at),
+                    "keyword2" => $l->money,
+                    "keyword3" => $l->money*$l->rate*$l->duration,
+                    "remark"   => "",
+                );
+                $messageId = $notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($l->wechat_id)->send();
+            }
         }
 
-        echo "Reminder Stop\n";
+        echo "Reminder Stop...\n";
 
     }
 
