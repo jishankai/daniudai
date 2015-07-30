869c85fa27a09f5061e4b952a7bd1ed9332d66a4
skji <jishankai@gmail.com>
2015-07-30 11:29:09 +0800

    update

--- frontend/controllers/LoanController.php
+++ frontend/controllers/LoanController.php
@@ -197,12 +197,12 @@ class LoanController extends \yii\web\Controller
         $b1 = Bank::find()
             ->select('bank.*')
             ->leftJoin('loan', '`loan`.`wechat_id`=`bank`.`wechat_id`')
-            ->where(['and', "bank.card=$card", 'loan.status>1'])
+            ->where(['and', "bank.card=$card", 'loan.status>1 and loan.status!=4'])
             ->one();
         $b1 = Bank::find()
             ->select('bank.*')
             ->leftJoin('loan', '`loan`.`wechat_id`=`bank`.`wechat_id`')
-            ->where(['and', "bank.cid=$cid", 'loan.status>1'])
+            ->where(['and', "bank.cid=$cid", 'loan.status>1 and loan.status!=4'])
             ->one();
         if (isset($b1)||isset($b2)) {
             $resCode = '0000';
