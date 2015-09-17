72e76bc262b229d3da8bad6effe3d37b3bff851c
季善凯 <jishankai@qq.com>
2015-09-17 19:57:16 +0800

    update stu_id

--- frontend/web/js/person-information.js
+++ frontend/web/js/person-information.js
@@ -89,7 +89,7 @@ $(function(){
 		if(falg!=-1){
 			stu_id= $("#stu_id").val();
 			/*RegCellStuid = /^([0-9]*)?$/;*/
-			RegCellStuid =/^(\d{8}|\d{10})$/;
+		        RegCellStuid =/^(\d{8}|\d{10}|\d{12})$/;
 			falg1=stu_id.search(RegCellStuid);
 			stu_len=stu_id.length;
 			if(falg1==-1 || stu_len!=8 && stu_len!=10 && stu_len!=12){
