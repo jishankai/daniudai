b491a98e3127087850d0edfd4768ef57942941de
季善凯 <jishankai@qq.com>
2015-10-30 14:51:29 +0800

    update

--- vendor/overtrue/wechat/src/Wechat/QRCode.php
+++ vendor/overtrue/wechat/src/Wechat/QRCode.php
@@ -160,7 +160,7 @@ class QRCode
 
         $params = array(
                    'action_name' => $actionName,
-                   'action_info' => $actionInfo,
+                   'action_info' => array('scene' => $actionInfo),
                   );
 
         if ($temporary) {
